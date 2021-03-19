caa289f86f4bbc9dcc199049e5c97620904c6fcc
Marco <marco@serprogramador.es>
2021-03-18 22:22:02 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# VSCode for [Insomnia](http://insomnia.rest)
+
+> A dark theme for [Insomnia](http://insomnia.rest)
+
+![Screenshot](./screenshot.png)
+
+## Install
+
+All instructions can be found at [Installation instructions](INSTALL.md)
+
+
+## License
+
+[MIT License](./LICENSE)
+
+## Support me
+
+
+[![Marco Maldonado](https://avatars3.githubusercontent.com/u/2360493?v=5&s=70)](https://github.com/mxrck) |
+:---: |
+[Marco Maldonado](https://github.com/mxrck) |
+
+You can [follow me on Twitter](https://twitter.com/_mxrck), [buy me a coffee](https://www.paypal.me/animechannel/5usd) or [support me on Patreon](https://www.patreon.com/user?u=859275)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ themes/vscode.js
@@ -0,0 +1,117 @@
+module.exports = {
+	name: 'vscode',
+	displayName: 'VSCode',
+	theme: {
+		rawCss: `
+		.notice.error {
+			border-style: unset;
+			background-color: var(--color-danger);
+			color: white;
+		}
+		.notice.success {
+			border-style: unset;
+			background-color: var(--color-success);
+			color: black;
+		}
+		.notice.warning {
+			border-style: unset;
+			background-color: var(--color-warning);
+			color: black;
+		}
+		.notice.info {
+			border-style: unset;
+			background-color: var(--color-info);
+			color: black;
+		}
+		`,
+		background: {
+			default: '#252526',
+			success: '#50fa7b',
+			notice: '#f1fa8c',
+			warning: '#ffb86c',
+			danger: '#ff5555',
+			surprise: '#35A0F3',
+			info: '#8be9fd'
+		},
+		foreground: {
+			default: '#f8f8f2',
+			success: '#282a36',
+			notice: '#282a36',
+			warning: '#282a36',
+			danger: '#282a36',
+			surprise: '#282a36',
+			info: '#282a36'
+		},
+		highlight: {
+			md: '#252526', // '#1E1E1E',
+		},
+		styles: {
+			sidebar: {
+				background: {
+					default: '#252526'
+				},
+				highlight: {
+					default: '#cccccc',
+					xxs: 'rgba(98, 98, 98, 0.05)',
+					xs: 'rgba(98, 98, 98, 0.1)',
+					sm: 'rgba(98, 98, 98, 0.2)',
+					md: 'rgba(98, 98, 98, 0.4)',
+					lg: 'rgba(98, 98, 98, 0.6)',
+					xl: 'rgba(98, 98, 98, 0.8)'
+				},
+				foreground: {
+					default: '#cccccc',
+				},
+			},
+			pane: {
+				// Editor 
+				background: {
+					default: '#1E1E1E',
+					success: '#9CDCFE',
+					notice: '#CE9178',
+					warning: '#f1fa8c',
+					danger: '#ff5555',
+					surprise: '#35A0F3',
+					info: '#B5CEA8'
+				},
+				highlight: {
+					default: '#8F8F8F',
+					xs: '#37373D',
+					//md: '#444444',
+					//md: '#151515',
+					md: '#252526',
+					lg: '#A9A9A9',
+					xl: '#6D6D6D'
+				},
+			},
+			transparentOverlay: {
+				background: {
+					default: 'rgba(40, 40, 40, 0.5)',
+				}
+			},
+			dialog: {
+				background: {
+					default: '#252526',
+				}
+			},
+			link: {
+				background: {
+					surprise: '#CE9178',
+				},
+			},
+			sidebarList: {
+				background: {
+					success: '#B5CEA8',
+					surprise: '#35A0F3',
+				},
+				foreground: {
+					default: '#cccccc',
+				},
+				highlight: {
+					default: '#cccccc',
+					xs: '#37373D',
+				},
+			},
+		}
+	}
+}
